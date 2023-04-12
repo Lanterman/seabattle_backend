5b8e07d052adf3b1bd0b0a9f21b721edb0341a7a
Lanterman <splen1011@mail.ru>
2023-04-12 20:14:57 +0300

    Fixed bug with clearing enemy board

--- src/game/services.py
+++ src/game/services.py
@@ -4,7 +4,16 @@ column_name_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 def clear_enemy_board(user, boards) -> tuple:
     """Сlears the enemy table from ships and the space around them"""
 
-    index, enemy_board = (1, boards[1]) if boards[0]["user_id"] == user.id else (0, boards[0])
+    if boards[0]["user_id"]:
+        if boards[0]["user_id"] == user.id:
+            index, enemy_board = (1, boards[1])
+        else:
+            index, enemy_board = (0, boards[0])
+    else:
+        if boards[1]["user_id"] == user.id:
+            index, enemy_board = (0, boards[0])
+        else:
+            index, enemy_board = (1, boards[1])
 
     for key, value in enemy_board.items():
         if key in column_name_list:
